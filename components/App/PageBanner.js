d6b48f02387729e34571c592de10044ce003593c
Chinesebread <76460624+ChineseBread@users.noreply.github.com>
2022-08-30 16:46:58 +0800

    Update PageBanner.js

--- components/App/PageBanner.js
+++ components/App/PageBanner.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import CoverDataRequest from "../../uitls/request/CoverDataRequest";
 
 function PageBanner({url}) {
-    return url && <Image alt='AD110' src={CoverDataRequest.getCoverByUrl(url)} width={900} height={140} layout='responsive'/>
+    return url && <Image alt='AD110' quality={10} src={CoverDataRequest.getCoverByUrl(url)} width={900} height={140} layout='responsive'/>
 }
 
 export default PageBanner;
