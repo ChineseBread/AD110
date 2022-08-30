e7fc61e8f7557ed3063d543f8fc1215a5b0417da
Chinesebread <76460624+ChineseBread@users.noreply.github.com>
2022-08-30 17:22:46 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -56,7 +56,7 @@ function Homepage({HomePageData: {HotClickLinkList,EditorRecommendLink,HomePageB
                                             src={CoverDataRequest.getCoverByUrl(logo_image) || UrlBanner}
                                             priority={false}
                                             layout='fill'
-                                            quality={10}
+                                            quality={1}
                                             // width={210}
                                             // height={66}
                                             // objectFit='contain'
