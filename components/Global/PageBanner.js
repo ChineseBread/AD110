57c2f0fb763a4d11760d947b141434dd6ae18f7e
Chinesebread <76460624+ChineseBread@users.noreply.github.com>
2022-08-30 17:52:04 +0800

    Update PageBanner.js

--- components/Global/PageBanner.js
+++ components/Global/PageBanner.js
@@ -2,7 +2,7 @@ import Image from "next/image";
 import CoverDataRequest from "../../uitls/request/CoverDataRequest";
 
 function PageBanner({url}) {
-    return url && <div className='page-banner'><a href={`/api/banner/redirect_by_image?url=${url}`} target='_blank'><Image quality={10} alt='AD110' src={CoverDataRequest.getCoverByUrl(url)} objectFit='cover' width={900} height={100} layout='responsive'/></a></div>
+    return url && <div className='page-banner'><a href={`/api/banner/redirect_by_image?url=${url}`} target='_blank'><Image quality={20} alt='AD110' src={CoverDataRequest.getCoverByUrl(url)} objectFit='cover' width={900} height={100} layout='responsive'/></a></div>
 }
 
 export default PageBanner;
