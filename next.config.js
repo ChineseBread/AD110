48eb8c9db43e4efd9d9ff6ac57b1dce0332d25c5
Chinesebread <76460624+ChineseBread@users.noreply.github.com>
2022-09-01 12:48:03 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,51 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async headers() {
-    return [
-      {
-        source: '/:slug*',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'must-revalidate,maxage=3600',
-          },
-        ],
-      },
-      {
-        source: '/section',
-        has: [
-          {
-            type: 'query',
-            key: 'articleid',
-            value: '^[0-9]*$',
-          },
-        ],
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'must-revalidate,maxage=21600'
-          }
-        ]
-      },
-      {
-        source: '/news/check',
-        has: [
-          {
-            type: 'query',
-            key: 'newsid',
-            value: '^[0-9]*$',
-          },
-        ],
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'must-revalidate,maxage=21600'
-          }
-        ]
-      }
-    ]
-  },
   swcMinify:true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
